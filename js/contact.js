d9210418b7286e452c02e9d929a250e2cf8fb0f3
Sagar Singh <singhkgp47@gmail.com>
2020-06-11 11:07:14 +0530

    some bugs are fixed

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,6 @@
 $(function () {
 
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
-
+    
     $('#contact-form').validator();
 
 
@@ -21,20 +18,14 @@ $(function () {
                 data: $(this).serialize(),
                 success: function (data)
                 {
-                    // data = JSON object that contact.php returns
 
-                    // we recieve the type of the message: success x danger and apply it to the 
                     var messageAlert = 'alert-' + data.type;
                     var messageText = data.message;
 
-                    // let's compose Bootstrap alert box HTML
                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
                     
-                    // If we have messageAlert and messageText
                     if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
                         $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
                         $('#contact-form')[0].reset();
                     }
                 }
